90c1f673795c261c7cc268fa13c2607c7e8a6708
Jose Motta <josemotta@bampli.com>
2021-11-12 14:45:54 -0300

    comecando com React Hook - useState

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -24,6 +24,12 @@ const useStyles = makeStyles((theme: Theme) => ({
 export const Navbar: React.FC = () => {
     const classes = useStyles();
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleOpen = (event: any) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
@@ -33,15 +39,21 @@ export const Navbar: React.FC = () => {
                     aria-label="open drawer"
                     aria-controls="menu-appbar"
                     aria-haspopup="true"
+                    onClick={handleOpen}
                 >
                     <MenuIcon />
                 </IconButton>
                 
                 <Menu
                     id="menu-appbar"
-                    open={false}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+                    transformOrigin={{vertical: 'top', horizontal: 'center'}}
+                    getContentAnchorEl={null}
                 >
-                    <MenuItem>
+                    <MenuItem onClick={handleClose}>
                         Categories
                     </MenuItem>
                 </Menu>
