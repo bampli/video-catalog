da634f7e8bb655d2fb42670e26b6f437855d1af0
Jose Motta <josemotta@bampli.com>
2021-11-12 12:44:04 -0300

    criando menu no Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import './App.css';
-import { Button } from '@material-ui/core';
 import { Navbar } from './components/Navbar';
 
 const App: React.FC = () => {
   return (
-    <Navbar/>
+    <Navbar />
   );
 }
 
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { AppBar, Button, makeStyles, Theme, Toolbar, Typography } from '@material-ui/core';
+import {AppBar, Button, IconButton, makeStyles, Menu, MenuItem, Theme, Toolbar, Typography} from '@material-ui/core';
 import logo from '../../static/img/logo.png';
+import MenuIcon from '@material-ui/icons/Menu';
 
 // available sizes: xs, sm, md, lg, xl.
 
-const useStyles = makeStyles( (theme: Theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
         backgroundColor: "#000000"
     },
@@ -23,14 +24,32 @@ const useStyles = makeStyles( (theme: Theme) => ({
 export const Navbar: React.FC = () => {
     const classes = useStyles();
 
-  return (
-    <AppBar>
-        <Toolbar className={classes.toolbar}>
-            <Typography className={classes.title}>
-                <img src={logo} alt="CodeFlix" className={classes.logo}/>
-            </Typography>
-            <Button color="inherit">Login</Button>
-        </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar>
+            <Toolbar className={classes.toolbar}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                >
+                    <MenuIcon />
+                </IconButton>
+                
+                <Menu
+                    id="menu-appbar"
+                    open={false}
+                >
+                    <MenuItem>
+                        Categories
+                    </MenuItem>
+                </Menu>
+                <Typography className={classes.title}>
+                    <img src={logo} alt="CodeFlix" className={classes.logo} />
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+    );
 }
