0d8a7bb62fa1bb779cd292fa8e3bbf95be152790
Jose Motta <josemotta@bampli.com>
2021-11-16 17:26:30 -0300

    criando componente de listagem

---
+++ frontend/src/pages/category/Table.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+// import { Page } from '../../components/Page';
+// import { Box, Fab } from '@material-ui/core';
+// import { Link } from "react-router-dom";
+// import AddIcon from '@material-ui/icons/Add';
+
+const Table = () => {
+    return (
+        <div>
+            tabela
+        </div>
+    );
+}
+
+export default Table;
