ec63d0486bcebdabe2805f3393c9c81d9e1a68a1
Jose Motta <josemotta@bampli.com>
2021-11-16 19:35:26 -0300

    Hook - Use Effect

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,14 +1,53 @@
-import * as React from 'react';
-// import { Page } from '../../components/Page';
-// import { Box, Fab } from '@material-ui/core';
-// import { Link } from "react-router-dom";
-// import AddIcon from '@material-ui/icons/Add';
+import React, { useState, useEffect } from 'react';
+import MUIDataTable, { MUIDataTableColumn } from "mui-datatables";
 
-const Table = () => {
+const columnsDefinition: MUIDataTableColumn[] = [
+    {
+        name: "name",
+        label: "Nome"
+    },
+    {
+        name: "is_active",
+        label: "Ativo?"
+    },
+    {
+        name: "created_at",
+        label: "Criado em"
+    },
+];
+const data = [
+    { name: "teste1", is_active: true, created_at: "2021-11-10" },
+    { name: "teste2", is_active: false, created_at: "2021-11-11" },
+    { name: "teste3", is_active: true, created_at: "2021-11-12" },
+    { name: "teste4", is_active: false, created_at: "2021-11-13" },
+    { name: "teste5", is_active: true, created_at: "2021-11-16" },
+];
+
+type Props = {};
+
+const Table = (props: Props) => {
+
+    const [count, setCount] = useState(0);
+
+    //componentDidMount
+    useEffect(() => {
+        console.log('qualquer coisa')
+        return () => console.log('componente também desmontado')
+    }, []);
+    //componentDidUpdate & componentWillUnmount
+    useEffect(() => {
+        console.log(count)
+        return () => console.log('componente desmontado')
+    }, [count]);
+
+    // return (
+    //     <MUIDataTable
+    //         title="Listagem de categorias"
+    //         columns={columnsDefinition}
+    //         data={data}
+    //     />
     return (
-        <div>
-            tabela
-        </div>
+        <button onClick={() => setCount(count+1)}>{count}</button>
     );
 }
 
