6c53b4b401fcf036938d68bbb13d16a7a683b341
Jose Motta <josemotta@bampli.com>
2021-11-19 10:42:54 -0300

    personalizando coluna de ativos

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MUIDataTable, { MUIDataTableColumn } from "mui-datatables";
 import { httpVideo } from '../../util/http';
+import { Chip } from "@material-ui/core";
 
 const columnsDefinition: MUIDataTableColumn[] = [
     {
@@ -9,7 +10,12 @@ const columnsDefinition: MUIDataTableColumn[] = [
     },
     {
         name: "is_active",
-        label: "Ativo?"
+        label: "Ativo?",
+        options: {
+            customBodyRender(value, tableMeta, updateValue) {
+                return value ? <Chip label="Sim" color="primary" /> : <Chip label="Não" color="secondary" />;
+            }
+        }
     },
     {
         name: "created_at",
