218c4eca8358c6db8182920f00afdf8cf9053efd
Jose Motta <josemotta@bampli.com>
2022-01-04 17:51:06 -0300

    criando hook personalizado para organizar sistema busca

--- frontend/src/hooks/useFilter.ts
+++ frontend/src/hooks/useFilter.ts
@@ -2,6 +2,7 @@ import { useReducer, useState } from 'react';
 import reducer, { INITIAL_STATE } from "../store/filter";
 
 export default function useFilter() {
+    //console.log("useFilter");
     const [filterState, dispatch] = useReducer(reducer, INITIAL_STATE);
     const [totalRecords, setTotalRecords] = useState<number>(0);
 
