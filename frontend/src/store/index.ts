fc1281c2e89fa55383e4071b298cb3d01cb0df17
Jose Motta <josemotta@bampli.com>
2022-02-15 15:56:44 -0300

    declarando saga upload & rootSaga

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,20 +1,16 @@
-//import {createStore, applyMiddleware, combineReducers} from "redux";
-import {createStore, combineReducers} from "redux";
-//import createSagaMiddleware from 'redux-saga';
+import {createStore, applyMiddleware, combineReducers} from "redux";
+import createSagaMiddleware from 'redux-saga';
 import upload from "./upload";
-//import rootSaga from "./root-saga";
+import rootSaga from "./root-saga";
 
-
-//const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     combineReducers({
         upload
     }),
-    //applyMiddleware(sagaMiddleware)
+    applyMiddleware(sagaMiddleware)
 );
 
-//console.log("store->", store);
-
-//sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
 
 export default store;
