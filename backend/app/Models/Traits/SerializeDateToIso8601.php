2f4b11c25bfbd32f414cf52a07c96799ad287034
Jose Motta <josemotta@bampli.com>
2022-03-21 13:02:33 -0300

    fix date format ISO8601 - utilizando servico validacao fixtures e sincronizacao

--- backend/app/Models/Traits/SerializeDateToIso8601.php
+++ backend/app/Models/Traits/SerializeDateToIso8601.php
@@ -7,6 +7,6 @@ namespace App\Models\Traits;
 trait SerializeDateToIso8601
 {
     protected function serializeDate(\DateTimeInterface $date){
-        return $date->format(\DateTime::ISO8601);
+        return $date->format(\DateTime::ISO8601);   // 'Y-m-d\TH:1:s T'
     }
 }
